b8f35a11e17aead4e3d2b7045e75877f1ff514f9
needkofea <170401838+needkofea@users.noreply.github.com>
2024-12-07 21:10:49 +0800

    update install script

--- sddm/install_theme.sh
+++ sddm/install_theme.sh
@@ -4,7 +4,6 @@ pacman -S sddm qt5-graphicaleffects qt5-svg qt5-quickcontrols2
 
 # Get the directory of the current script
 SCRIPT_DIR=$(dirname "$(realpath "$0")" )
-
-ln -sf "$SCRIPT_DIR/nightcore/corners" "/usr/share/sddm/themes/nightcore"
-chmod 755 "/usr/share/sddm/themes/nightcore"
-
+echo Symlinking "$SCRIPT_DIR/nightcore/corners" to "/usr/share/sddm/themes/nightcore"
+ln -snf "$SCRIPT_DIR/nightcore/corners" "/usr/share/sddm/themes/nightcore"
+echo Success
