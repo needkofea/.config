936b92a6624ffc343c029797bf000359c31891b2
needkofea <170401838+needkofea@users.noreply.github.com>
2024-12-10 22:21:12 +0800

    updated notification theme

--- scripts/toggle-mute.sh
+++ scripts/toggle-mute.sh
@@ -3,8 +3,8 @@
 pamixer --default-source -t
 if  [[ $(pamixer --default-source --get-mute) == "true" ]]; then
     echo "Toggle Mute: Muted default mic";
-    notify-send "Muted Microphone" 
+    notify-send -a "Toggle Mute" "Muted" -t 2000 -u low
 else
     echo "Toggle Mute: Unmuted default mic";
-    notify-send "Unmuted Microphone"
+    notify-send -a "Toggle Mute" "Unmuted" -t 2000 -u low
 fi
