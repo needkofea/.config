2f975d1d0a8cd17f063f1fbd0afa662ae1ef9c45
needkofea <170401838+needkofea@users.noreply.github.com>
2024-12-13 22:21:49 +0800

    fix notifs

--- rofi/scripts/clipboard.sh
+++ rofi/scripts/clipboard.sh
@@ -4,14 +4,13 @@ dir="$HOME/.config/rofi/menus"
 theme='clipboard'
 
 ## Run
-selected=$(cliphist list | rofi -dmenu -theme "$dir/$theme")
+selected=$(cliphist list | rofi -dmenu -m -2 -show -theme "$dir/$theme")
 
-if [ -z "$selected"]; then
+if [ -z "$selected" ]; then
     exit 1
 fi
 
-preview=$(echo "$selected" | sed 's/^[^\t]*\t//')
-
 printf "$selected" | cliphist decode | wl-copy
+preview=$( wl-paste )
 
-notify-send "Copied to clipboard" "$preview" -a "Clipboard History"
+notify-send "Copied to clipboard" "\`$preview\`" -a "Clipboard History"
