d3c22f3a65ecde7b05e4c3776925e43789c3753b
needkofea <170401838+needkofea@users.noreply.github.com>
2024-12-13 21:56:12 +0800

    Dont send notification when no selection

--- rofi/scripts/clipboard.sh
+++ rofi/scripts/clipboard.sh
@@ -5,6 +5,11 @@ theme='clipboard'
 
 ## Run
 selected=$(cliphist list | rofi -dmenu -theme "$dir/$theme")
+
+if [ -z "$selected"]; then
+    exit 1
+fi
+
 preview=$(echo "$selected" | sed 's/^[^\t]*\t//')
 
 printf "$selected" | cliphist decode | wl-copy
