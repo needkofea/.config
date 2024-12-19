3f740b964bd1548e813538b15baccc74229e5835
needkofea <170401838+needkofea@users.noreply.github.com>
2024-12-19 21:49:03 +0800

    volume notifications

--- scripts/toggle-mute.sh
+++ scripts/toggle-mute.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 
+_DIR=$(dirname "$(realpath "$0")")
+
+# Check if pamixer is installed
+if ! command -v pamixer &>/dev/null; then
+    echo "pamixer is not installed. Please install it first."
+    exit 1
+fi
+
+TEMP_FILE="/tmp/volume-output-last-notification.tmp"
+PREV_ID=$(cat "$TEMP_FILE" 2>/dev/null)
+if [[ -n "$PREV_ID" ]]; then
+    NOTIFY_ARGS="-r $PREV_ID"
+fi
+
 pamixer --default-source -t
 if  [[ $(pamixer --default-source --get-mute) == "true" ]]; then
     echo "Toggle Mute: Muted default mic";
-    notify-send -a "Toggle Mute" "Muted" -t 2000 -u low
+    NOTIFY_ID=$(notify-send -a "Toggle Mute" "Muted" -t 2000 -u low $NOTIFY_ARGS -p -i "$_DIR/IconoirMicrophoneMuteSolid.svg")
 else
     echo "Toggle Mute: Unmuted default mic";
-    notify-send -a "Toggle Mute" "Unmuted" -t 2000 -u low
+    NOTIFY_ID=$(notify-send -a "Toggle Mute" "Unmuted" -t 2000 -u low $NOTIFY_ARGS -p -i "$_DIR/IconoirMicrophoneSolid.svg")
 fi
+
+
+echo $NOTIFY_ID > $TEMP_FILE
